38dc59f43117908427a3a5655c6b797b2e6c0bc9
Patrycja-dz <patrycja.dziemianko@wp.pl>
2021-05-08 12:11:23 +0200

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,48 @@
 {
-    const tasks = [
-        {
-            content: "zupa",
+    const tasks = [{
+            content: "omomom",
             done: true,
         },
         {
-            content: "gzik",
-            done:"false"
-        }
-    ]
+            content: "buebeu",
+            done: false,
+        },
+    ];
 
 
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
             htmlString += `
-           <li>
+           <li
+           ${task.done ? "list__item--done" : "list__item"}
+           >
+           <button class = "list__button js-done">✅</button>
+           <button class = "list__button list__button--remove js-remove">🗑️</button>
            ${task.content}
            </li>`;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
     }
-    const init = () => {
-        render()
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
     }
-    init()
-}
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+
+        };
+        addNewTask(newTaskContent);
+    };
+    const init = () => {
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit)
+        render()
+    };
+    init();
+};
