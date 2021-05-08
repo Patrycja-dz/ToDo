83f290d3e7a4465b8d877a8597bcf5547037399c
Patrycja-dz <patrycja.dziemianko@wp.pl>
2021-05-08 12:24:47 +0200

    adding remove events and function

--- js/script.js
+++ js/script.js
@@ -23,12 +23,23 @@
            </li>`;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index)
+            })
+
+        })
     }
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
         render();
+    }
+    const removeTask = index => {
+        tasks.splice(index, 1);
+        render()
     }
     const onFormSubmit = (event) => {
         event.preventDefault();
